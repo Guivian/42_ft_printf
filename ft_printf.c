445a72166efaf49b9a6a0ce43d529bf1b53cfd0c
Luis guilherme Barbosa viana <lbarbosa@c1r16s1.42lisboa.com>
2022-03-01 16:50:58 +0000

    ft_printf OK!

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lbarbosa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 18:20:48 by lbarbosa          #+#    #+#             */
-/*   Updated: 2022/02/27 19:28:12 by lbarbosa         ###   ########.fr       */
+/*   Updated: 2022/03/01 16:42:37 by lbarbosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,7 @@ int	ft_printf_iteri(const char *format, va_list *args, int counter, int i)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				counter += ft_printf_c(va_arg(*args, int));
-			else if (format[i] == 's')
-				counter += ft_printf_s(va_arg(*args, char*));
-			else if (format[i] == 'p')
-				counter += ft_printf_p(va_arg(*args, unsigned long int));
-			else if (format[i] == 'd')
-				counter += ft_printf_di(va_arg(*args, int));
-			else if (format[i] == 'i')
-				counter += ft_printf_di(va_arg(*args, int));
-			else if (format[i] == 'u')
-				counter += ft_printf_u(va_arg(*args, unsigned int));
-			else if (format[i] == 'x')
-				counter += ft_printf_x(va_arg(*args, long));
-			else if (format[i] == 'X')
-				counter += ft_printf_mx(va_arg(*args, long));
-			else if (format[i] == '%')
-				counter += write(1, "%", 1);
+			counter += ft_printf_formatspecifier(format, args, i);
 		}
 		else
 			counter += write(1, &format[i], 1);
@@ -58,10 +41,26 @@ int	ft_printf_iteri(const char *format, va_list *args, int counter, int i)
 	return (counter);
 }
 
-/*int	main(void)
+int	ft_printf_formatspecifier(const char *format, va_list *args, int i)
 {
-	int len = 0;
-	int	len1 = 0;
-	len = ft_printf(" %d len = %d ", 0123456754321513561325, len);
-	len1 = printf("\n %d len = %d ", -2, len1);
-}*/
+	int	ret;
+
+	ret = 0;
+	if (format[i] == 'c')
+		ret += ft_printf_c(va_arg(*args, int));
+	else if (format[i] == 's')
+		ret += ft_printf_s(va_arg(*args, char *));
+	else if (format[i] == 'p')
+		ret += ft_printf_p(va_arg(*args, unsigned long int));
+	else if (format[i] == 'd' || format[i] == 'i')
+		ret += ft_printf_di(va_arg(*args, int));
+	else if (format[i] == 'u')
+		ret += ft_printf_u(va_arg(*args, unsigned int));
+	else if (format[i] == 'x')
+		ret += ft_printf_x(va_arg(*args, long));
+	else if (format[i] == 'X')
+		ret += ft_printf_mx(va_arg(*args, long));
+	else if (format[i] == '%')
+		ret += write(1, "%", 1);
+	return (ret);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lbarbosa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 18:19:21 by lbarbosa          #+#    #+#             */
-/*   Updated: 2022/02/27 19:38:38 by lbarbosa         ###   ########.fr       */
+/*   Updated: 2022/03/01 16:43:47 by lbarbosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 int	ft_printf(const char *format, ...);
 int	ft_printf_iteri(const char *format, va_list *args, int counter, int i);
+int	ft_printf_formatspecifier(const char *format, va_list *args, int i);
 int	ft_printf_c(char c);
 int	ft_printf_s(char *s);
 int	ft_printf_p(unsigned long int p);
